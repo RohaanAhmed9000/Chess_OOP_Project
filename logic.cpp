32dec735cd8035345c7ec9472f858ba4c030e5de
RohaanAhmed9000 <122354501+RohaanAhmed9000@users.noreply.github.com>
2023-11-07 00:25:34 -0800

    Add files via upload

---
+++ logic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+/*
+interaction part
+*/
+class Click  // where the click is and what it does
+{
+
+};
+
+class Messages // prompts
+{
+
+};
+class GameState // for running , user lost + user won
+{
+
+};
+int main ()
+{
+    return 0;
+}
