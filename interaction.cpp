7323af9c38a3128b1ebe024b410225258f23a0d7
RohaanAhmed9000 <122354501+RohaanAhmed9000@users.noreply.github.com>
2023-11-07 00:25:05 -0800

    Add files via upload

---
+++ interaction.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+/*
+we are using namespace std
+first letter of classes capital
+*/
+class Pieces 
+{
+    public:
+    void virtual get()=0; // just for purely virtual functionality , dummy function
+
+    private:
+
+    protected:
+
+
+};
+
+class Pawn: public Pieces // 99% inheritence public
+{
+
+};
+
+class Bishop: public Pieces // 99% inheritence public
+{
+
+};
+class Knight: public Pieces // 99% inheritence public
+{
+
+};
+class Rook: public Pieces // 99% inheritence public
+{
+
+};
+class King: public Pieces // 99% inheritence public
+{
+
+};
+class Queen: public Pieces // 99% inheritence public
+{
+
+};
+
+class Interaction
+{
+    
+};
+
+class Movement // 99% inheritence public
+{
+
+};
+
+class Board
+{
+
+};
+int main ()
+{
+    return 0;
+}
